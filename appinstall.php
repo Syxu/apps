8ef4f4368669ee1d610b47c8653337fccc3e7690
syxu <fcsfms@gmail.com>
2017-05-22 13:11:33 +0800

    add

---
+++ appinstall.php
@@ -0,0 +1,16 @@
+﻿<title>恶龙法则</title>
+<img src="http://1v5499909c.iask.in:8081/apps/ICON.png"  width="128" height="128" />
+<br><br/>
+<a>《恶龙法则》</a>
+<br><br/>
+<a>苹果用户 请使用Safari打开</a>
+<br><br/>
+<a href="itms-services://?action=download-manifest&url=https://raw.githubusercontent.com/Syxu/apps/master/apps.plist" id="text">安装iOS版本</a> 
+<br><br/>
+<a>安卓用户</a>
+<br><br/>
+<a href="http://1v5499909c.iask.in:8081/apps/dragonfantasy.apk">安装Andorid版本</a>
+<br><br/>
+<a>更新日志</a>
+<br><br/>
+<?php include("http://1v5499909c.iask.in:8081/apps/updatelog.html"); ?>
